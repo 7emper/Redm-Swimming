c59365b30a6af799a01f5eb342e09abc7d3ff1a1
7emper <138748490+7emper@users.noreply.github.com>
2023-07-06 02:36:55 -0400

    Update client.lua
    Just a bit more optimized

--- 7empers-swimming/client.lua
+++ 7empers-swimming/client.lua
@@ -1,17 +1,17 @@
+function CheckPlayerWaterStatus()
+    if IsEntityInWater(PlayerPedId()) then
+        local stam = GetAttributeCoreValue(PlayerPedId(), 1)
+        if stam > 2 then
+            Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, stam)
+        else
+            Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, 5)
+        end
+    end
+end
+
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-        -- Check if the player is in water
-        if IsEntityInWater(PlayerPedId()) then
-            stam = GetAttributeCoreValue(PlayerPedId(), 1)
-            if stam > 2 then
-                --print(stam)
-                Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, stam)
-            else
-                Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, 5)
-            end
-        else
-            -- Do nothing
-        end
+        CheckPlayerWaterStatus()
     end
 end)
