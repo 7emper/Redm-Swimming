9709131811c572259a484059443791bd11dd8bb9
7emper <138748490+7emper@users.noreply.github.com>
2023-07-06 00:49:37 -0400

    Update client.lua
    the original did work, but when out of bounds you would instantly be set to red core, (0 stamina core) so i pushed the if statement to check if the player is below 2 stamina core. If YES then replace with 5 core.

--- 7empers-swimming/client.lua
+++ 7empers-swimming/client.lua
@@ -4,8 +4,12 @@ Citizen.CreateThread(function()
         -- Check if the player is in water
         if IsEntityInWater(PlayerPedId()) then
             stam = GetAttributeCoreValue(PlayerPedId(), 1)
-            print(stam)
-            Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, stam)
+            if stam > 2 then
+                --print(stam)
+                Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, stam)
+            else
+                Citizen.InvokeNative(0xC6258F41D86676E0, PlayerPedId(), 1, 5)
+            end
         else
             -- Do nothing
         end
